b992525dadc70caa94ba0ea7825eda35e363940a
eliezer <elifred21@hotmail.com>
2022-06-22 21:23:58 -0300

    visto crear ussuario api, HTTP STATUS CODE, cree api hey

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # curso_API
+
+pagina de API: https://thecatapi.com/
+
+
+HTTP status codes : https://httpstatusdogs.com/
+
+1XX Respuestas Afirmativas
+2XX Respuestas satisfactorias
+3XX Re-direcciones
+4XX Error del cliente
+5XX Error de servidor - el backend fallo
+
+MI API_KEY: 75598d5f-19d1-4c23-b064-b76ce65b39b5
+
+MAQUETACION DEL PROYECTO
+
+metodos HTTP: 
+- GET, POST, PUT Y PATCH, DELETE
+
+quede en video 10
--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-console.log("hello, world")
+const API_URL_random = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5"; //aca usamos querys "?limit=3 q significa maximo de imagenes 3"
+const API_URL_favorites = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5";
 
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3"; //aca usamos querys "?limit=3 q significa maximo de imagenes 3"
+const spanEror = document.getElementById("error");
 
 
-
-    async function reload() { // funcion del boton onclick
-        const res = await fetch(API_URL);
+    async function loadRandomMichis() { // funcion del boton onclick
+        const res = await fetch(API_URL_random);
         const data = await res.json();
 
+            console.log("random")
             console.log(data)
-            const img1 = document.getElementById("img1");
-            const img2 = document.getElementById("img2");
-            const img3 = document.getElementById("img3");
             
-            img1.src = data[0].url;
-            img2.src = data[1].url;
-            img3.src = data[2].url;
+            if (res.status !== 200){
+                spanEror.innerHTML = "Hubo un error: " + res.status;
+            } else {
+                const img1 = document.getElementById("img1");
+                const img2 = document.getElementById("img2");
+                
+                
+                img1.src = data[0].url;
+                img2.src = data[1].url;
+            }
+
+            
+            
 
         }
 
-        reload(); //para q se carggue la imagen de una vez al abrir
+        async function loadFavoritesMichis() { // funcion del boton onclick
+            const res = await fetch(API_URL_favorites);
+            const data = await res.json();
+
+                console.log("favoritos")
+                console.log(data)
+                /*const img1 = document.getElementById("img1");
+                const img2 = document.getElementById("img2"); 
+                
+                
+                img1.src = data[0].url;
+                img2.src = data[1].url; */
+                
     
+            }
+
+        loadRandomMichis(); //para q se carggue la imagen de una vez al abrir
+        loadFavoritesMichis();
