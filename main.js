eceff5c13aaf7c227c1fe62bc29cc8add144af62
eliezer <elifred21@hotmail.com>
2022-06-23 20:52:27 -0300

    haciendo pruebas con el codigo del pofe

--- main.js
+++ main.js
@@ -1,70 +1,56 @@
-const API_URL_random = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5"; //aca usamos querys "?limit=3 q significa maximo de imagenes 3"
-const API_URL_favorites = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5";
 
-const spanError = document.getElementById("error");
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5';  //aca usamos querys "?limit=3 q significa maximo de imagenes 3
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5';
 
+const spanError = document.getElementById('error')
 
-    async function loadRandomMichis() { // funcion del boton onclick
-        const res = await fetch(API_URL_random);
-        const data = await res.json();
-            console.log("random")
-            console.log(data)
-            
-            if (res.status !== 200){
-                spanError.innerHTML = "Hubo un error: " + res.status;
-            } else {
-                const img1 = document.getElementById("img1");
-                const img2 = document.getElementById("img2");
-                
-                img1.src = data[0].url;
-                img2.src = data[1].url;
-            }
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log('Random')
+  console.log(data)
 
-            
-            
-
-        }
-
-        async function loadFavoriteMichis() { // funcion del boton onclick
-            const res = await fetch(API_URL_favorites);
-            const data = await res.json();
-
-                console.log("favoritos")
-                console.log(data)
-                /*const img1 = document.getElementById("img1");
-                const img2 = document.getElementById("img2"); 
-                
-                
-                img1.src = data[0].url;
-                img2.src = data[1].url; */
-            if (res.status !== 200) {
-                spanError.innerHTML = "Hubo un error: " + res.status + data.
-                Message;
-            }
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
     
-            }
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
+}
 
-        async function saveFavoriteMichis() {
-            const res = await fetch(API_URL_favorites, { //metodo POST
-                method: 'POST',
-                headers:{
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify ({
-                    image_id: 'MTUzNjQwNw'
-                }),
-            });
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOTITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
 
-            const data = await res.json();
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
 
-            console.log('save')
-            console.log(res)
+async function saveFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOTITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: 'dje'
+    }),
+  });
+  const data = await res.json();
 
-            if (res.status !== 200) {
-                spanError.innerHTML = "Hubo un error: " + res.status + data.
-                Message;
-            }
-        }
+  console.log('Save')
+  console.log(res)
 
-        loadRandomMichis(); //para q se cargue la imagen de una vez al abrir
-        loadFavoriteMichis();
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
