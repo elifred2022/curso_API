994b0053cc4853177ff92960a199bf0d839c5556
eliezer <elifred21@hotmail.com>
2022-06-23 22:52:26 -0300

    tuve q empezar de nuevo vi clase get hasta ahora va bien

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5';  //aca usamos querys "?limit=3 q significa maximo de imagenes 3
-const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=75598d5f-19d1-4c23-b064-b76ce65b39b5';
 
 const spanError = document.getElementById('error')
 
@@ -15,42 +15,27 @@ async function loadRandomMichis() {
   } else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    
+      
     img1.src = data[0].url;
     img2.src = data[1].url;
   }
+  
+  
 }
 
-async function loadFavouriteMichis() {
-  const res = await fetch(API_URL_FAVOTITES);
-  const data = await res.json();
-  console.log('Favoritos')
-  console.log(data)
 
-  if (res.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+async function loadFavoritesMichis() {
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+    console.log('Favoritos')
+    console.log(data)
+    
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+      } 
+    
   }
-}
-
-async function saveFavouriteMichis() {
-  const res = await fetch(API_URL_FAVOTITES, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      image_id: 'dje'
-    }),
-  });
-  const data = await res.json();
-
-  console.log('Save')
-  console.log(res)
-
-  if (res.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
-  }
-}
 
 loadRandomMichis();
-loadFavouriteMichis();
+loadFavoritesMichis();
+
